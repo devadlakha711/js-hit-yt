d8ffbfa8f57655d154eeed2b0a6f86070228bf12
devadlakha711 <165180934+devadlakha711@users.noreply.github.com>
2025-04-21 19:17:55 +0000

    datatypes and conversion

---
+++ 01_basics/02_dt.js
@@ -0,0 +1,22 @@
+//data types
+ "use strict";//treat all JS code as newer version
+//  alert( 3 + 3 ) //we are using node js not browser
+console.log(3+3) 
+console.log("Dev") //code readibility high
+
+let name="dev"
+let age=21
+let isLoggedIn=false
+let state=null
+//number -> 2 to power 53
+//bigint
+//string -> ""
+//boolean-> true/false
+//null->standalsone value
+//undefined
+//symbol ->unique
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
