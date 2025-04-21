8845d31e2c9571eaa74eac79b9eab9f5e5ad192a
devadlakha711 <165180934+devadlakha711@users.noreply.github.com>
2025-04-21 13:23:18 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 14453
+let accountEmail="devadlakha38@gmail.com" //use
+var accountPassword="12345" //scope problem ->dont use
+accountCity="Delhi" //not good tareeka
+let accountState;
+// accountId=133
+accountEmail="dev@gmail.com"
+accountPassword="711"
+accountCity="Faridabad"
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity,accountState]);
+// console.table([
+//     { Email: accountEmail, ID: accountId, Password: accountPassword, City: accountCity }
+//   ]);
+
