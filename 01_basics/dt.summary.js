6b50ec72a4c1ace854dcdbfcfe4f6e91ee32fbd7
devadlakha711 <165180934+devadlakha711@users.noreply.github.com>
2025-04-23 13:58:24 +0000

    datatype summary

---
+++ 01_basics/dt.summary.js
@@ -0,0 +1,10 @@
+ // Primitive
+ //7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+ 
+//Reference (non Primitive)
+//Array,Objects,Functions
+// const myFunction=function(){
+//     console.log("Hello");
+    
+//  }
+//  myFunction();//invoking it
